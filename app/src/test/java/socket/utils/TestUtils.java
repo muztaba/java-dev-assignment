e7893dbf937f89b26067c55d778a9751d7982b9f
seal <hasanat.muztaba@gmail.com>
2021-05-09 16:09:31 +0600

    Use mutable map

--- app/src/test/java/socket/utils/TestUtils.java
+++ app/src/test/java/socket/utils/TestUtils.java
@@ -3,6 +3,8 @@ package socket.utils;
 import com.google.common.collect.ImmutableMap;
 import socket.io.RequestObject;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class TestUtils {
@@ -12,7 +14,9 @@ public class TestUtils {
         requestObject.setMethod("findPrimes");
         requestObject.setManagerName("PrimeCalculationManager");
         int randomNum = ThreadLocalRandom.current().nextInt(1000, 2000 + 1);
-        requestObject.setArgs(ImmutableMap.of("n", String.valueOf(randomNum)));
+        Map<String, String> map = new HashMap<>();
+        map.put("n", String.valueOf(randomNum));
+        requestObject.setArgs(map);
         return requestObject;
     }
 
