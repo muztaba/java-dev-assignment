89664bf9b7f4b3681d9ad7690d3c6b93ea99eb08
seal <hasanat.muztaba@gmail.com>
2021-05-09 06:07:30 +0600

    Introduce ParamName to get method parameter names

---
+++ app/src/main/java/socket/server/manager/ParamName.java
@@ -0,0 +1,14 @@
+package socket.server.manager;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ParamName {
+
+    String name() default "";
+
+}
